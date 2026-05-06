2ce744cd836a179347553eb936af453889465e9c
wesleyfelix <wesley.felixdelima96@hotmail.com>
2026-05-05 21:51:11 -0300

    trabalhando com chamadas em APIs

---
+++ src/br/com/alura/screenmatch/PrincipalComBusca.java
@@ -0,0 +1,27 @@
+package br.com.alura.screenmatch;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digitem um filme para busca: ");
+        var busca = URLEncoder.encode(leitura.nextLine(), "UTF-8");
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=43d47f62";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
